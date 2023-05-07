27d4efd4d0c1d3f550ea9ba3350fc5b77e5f26ac
jerometrancart <jerome.trancart@gmail.com>
2023-05-07 18:53:54 +0200

    Created model, created a class library domain

---
+++ SelfieAWookies.Core.Selfies.Domain/Selfie.cs
@@ -0,0 +1,12 @@
+﻿namespace SelfieAWookie.API.UI
+{
+    /// <summary>
+    /// Represent a selfie with a wookie linked
+    /// </summary>
+    public class Selfie
+    {
+        #region Properties
+        public int Id { get; set; }
+        #endregion
+    }
+}
