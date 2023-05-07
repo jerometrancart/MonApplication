31e78f80e3d48d73ccdf6a6babcfaf1780028c8b
jerometrancart <jerome.trancart@gmail.com>
2023-05-07 22:07:52 +0200

    added a class library for ef core to manypulate tables in db

--- MonApplication/Controllers/SelfiesController.cs
+++ MonApplication/Controllers/SelfiesController.cs
@@ -8,10 +8,23 @@ namespace SelfieAWookie.API.UI.Controllers
     public class SelfiesController : ControllerBase
     {
         #region Public methods
+        //[HttpGet]
+        //public IEnumerable<Selfie> TestAMoi()
+        //{
+        //    return Enumerable.Range(1, 10).Select(item => new Selfie() { Id = item });
+        //}
+
         [HttpGet]
-        public IEnumerable<Selfie> TestAMoi()
+        public IActionResult TestAMoi()
         {
-            return Enumerable.Range(1, 10).Select(item => new Selfie() { Id = item });
+            // modele = what i want as data is return
+            var model = Enumerable.Range(1, 10).Select(item => new Selfie() { Id = item });
+
+            // return a status code that can be interpreted by front
+
+            // return this.StatusCode(StatusCodes.Status200OK);
+
+            return this.Ok(model);
         }
         #endregion
     }
---
+++ SelfieAWookies.Core.Selfies.Infrastructure/Data/TypeConfiguration/WookieEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SelfieAWookie.API.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SelfieAWookies.Core.Selfies.Domain;
+
+namespace SelfieAWookies.Core.Selfies.Infrastructure.Data.TypeConfiguration
+{
+    /// <summary>
+    /// Define all that is needed for the wookie table
+    /// </summary>
+    internal class WookieEntityTypeConfiguration : IEntityTypeConfiguration<Wookie>
+    {
+        #region Public methods
+        public void Configure(EntityTypeBuilder<Wookie> builder)
+        {
+            builder.ToTable("Wookie");
+            builder.HasKey(item => item.Id);
+            builder.HasMany(item => item.Selfies);
+        }
+        #endregion
+    }
+}
