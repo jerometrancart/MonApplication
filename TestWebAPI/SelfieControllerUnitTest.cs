5545e53772e4b7e9422bd57f7ee1491b23dd9017
jerometrancart <jerome.trancart@gmail.com>
2023-05-10 05:31:54 +0200

    Unit test updated

--- TestWebAPI/SelfieControllerUnitTest.cs
+++ TestWebAPI/SelfieControllerUnitTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using SelfieAWookie.API.UI.Controllers;
 
 namespace TestWebAPI
@@ -9,13 +10,16 @@ namespace TestWebAPI
         public void Should()
         {
             // ARRANGE
-            var controller = new SelfiesController();
+            var controller = new SelfiesController(null);
             // ACT
             var result = controller.TestAMoi();
             // ASSERT
 
             Assert.NotNull(result);
-            Assert.True(result.GetEnumerator().MoveNext());
+            Assert.IsType<OkObjectResult>(result);
+
+            OkObjectResult okResult = result as OkObjectResult;
+            Assert.NotNull(okResult.Value);
         }
         #endregion
     }
