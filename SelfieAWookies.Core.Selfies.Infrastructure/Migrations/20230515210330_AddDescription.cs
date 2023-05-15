ae744cd57229c3b18cd9c328f2ea201eeabefbd7
jerometrancart <jerome.trancart@gmail.com>
2023-05-15 23:11:49 +0200

    Added a column in db from code

---
+++ SelfieAWookies.Core.Selfies.Infrastructure/Migrations/20230515210330_AddDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SelfieAWookies.Core.Selfies.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Selfie",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Selfie");
+        }
+    }
+}
