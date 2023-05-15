8be5e111c66f5cf61e2bf13886ce59cdf02d18dd
jerometrancart <jerome.trancart@gmail.com>
2023-05-15 11:12:29 +0200

    Added design factory, dependencies, settings

--- SelfieAWookies.Core.Selfies.Infrastructure/Data/SelfiesContextFactory.cs
+++ SelfieAWookies.Core.Selfies.Infrastructure/Data/SelfiesContextFactory.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,29 @@ using System.Threading.Tasks;
 
 namespace SelfieAWookies.Core.Selfies.Infrastructures.Data
 {
-    internal class SelfiesContextFactory
+    internal class SelfiesContextFactory : IDesignTimeDbContextFactory<SelfiesContext>
     {
+        /// <summary>
+        /// implement design factory
+        /// </summary>
+        
+        #region Public methods
+        public SelfiesContext CreateDbContext(string[] args)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+
+            configurationBuilder.AddJsonFile(Path.Combine(Directory.GetCurrentDirectory(), "Settings", "appSettings.json"));
+
+            IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+            DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+
+            builder.UseSqlServer(configurationRoot.GetConnectionString("SelfiesDatabase"));
+
+            SelfiesContext context = new SelfiesContext();
+
+            return context;
+        }
+        #endregion
     }
 }
